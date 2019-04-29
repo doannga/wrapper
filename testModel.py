483e9bbdb46a50f937aa29d982eebd286cf976fc
doannga <doannga1234@gmail.com>
2019-04-29 23:28:54 +0700

    Change file test Model

--- testModel.py
+++ testModel.py
@@ -30,9 +30,29 @@ def get_json(filePath):
                 })
     # json.dump(data, open('./Result/text1.txt','w', encoding= 'utf-8'), indent = 4)
     return data
-def testModel(filePath):
-    #  Read input data
-    new_data = get_json(filePath)
+# def testModel(filePath):
+#     #  Read input data
+#     new_data = get_json(filePath)
+#     classifier = pickle.load(open('trained_model/logistic_model.pk','rb'))
+
+#     # tf-idf
+#     vectorizer = pickle.load(open(settings.VECTOR_EMBEDDING,'rb'))
+#     data_features = []
+#     i = 0
+#     result = []
+#     for i in range(len(new_data)):
+#         data_features.append(' '.join(NLP(text=new_data[i].get('content')).get_words_feature()))
+#         features = vectorizer.transform(data_features)
+#         result_label = classifier.predict(features)
+#         xpath = new_data[i].get('xpath')
+#         result.append({
+#             'xpath': xpath,
+#             'label': result_label[i],
+#             'content': new_data[i].get('content')
+#         })
+#     return result
+def wrapper( list_xpath_content, list_mismath_attribute):
+
     classifier = pickle.load(open('trained_model/logistic_model.pk','rb'))
 
     # tf-idf
@@ -40,19 +60,17 @@ def testModel(filePath):
     data_features = []
     i = 0
     result = []
-    for i in range(len(new_data)):
-        data_features.append(' '.join(NLP(text=new_data[i].get('content')).get_words_feature()))
+    for i in range(len(list_xpath_content)):
+        data_features.append(' '.join(NLP(text=list_xpath_content[i][1].get_words_feature()))
         features = vectorizer.transform(data_features)
         result_label = classifier.predict(features)
-        xpath = new_data[i].get('xpath')
-        result.append({
-            'xpath': xpath,
-            'label': result_label[i],
-            'content': new_data[i].get('content')
-        })
+        xpath = list_xpath_content[i][0]
+        if result_label in list_mismath_attribute:
+            result.append([result_label,xpath])
+
     return result
 
-if __name__ == '__main__':
-    result = testModel('./Resources/data/data_empty.json')
-    with open('Result/result1.text',encoding = 'utf-8', mode = 'w') as f:
-        json.dump(result, f,indent = 4, ensure_ascii=False)
+# if __name__ == '__main__':
+#     result = testModel('./Resources/data/data_empty.json')
+#     with open('Result/result1.text',encoding = 'utf-8', mode = 'w') as f:
+#         json.dump(result, f,indent = 4, ensure_ascii=False)
